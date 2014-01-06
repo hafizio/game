1df92fb1607fcd7dc9bee85fe3dc7400379fed8d
Hafiz <ibnukamy@gmail.com>
2014-01-06 17:05:27 +0800

    Spike: Hero attacks monster

---
+++ spike.rb
@@ -0,0 +1,52 @@
+class Hero
+  attr_reader :health, :strength
+
+  def initialize(health, strength)
+    @health = health
+    @strength = strength #integer
+  end
+
+  def attack(monster)
+    dice = [] #rand(@strength)
+    strength.times { dice << 1 + rand(6) }
+    successes = dice.count { |die| die > 4 }
+    if successes >= monster.toughness
+      return true
+    else
+      @health -= monster.damage
+      return false
+    end
+  end
+
+  def dead?
+    @health <= 0
+  end
+end
+
+class Monster
+  attr_reader :toughness, :damage
+
+  def initialize(toughness, damage)
+    @toughness = toughness
+    @damage = damage
+  end
+end
+
+
+hero = Hero.new 10, 5
+monster = Monster.new 2, 4
+
+until hero.attack(monster) || hero.dead?
+  puts "You missed. Monster dealt #{monster.damage}"
+  puts " Your curent health is #{hero.health}"
+end
+
+puts "====================================="
+
+if hero.dead?
+  puts "You LOST!"
+else
+  puts "You WON!"
+end
+
+puts "====================================="
