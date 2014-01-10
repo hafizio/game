6f52009d5bc266995efa372eadbae559a2b4b8da
Hafiz <ibnukamy@gmail.com>
2014-01-10 09:29:19 +0800

    Her0 class and spec refactoring

--- Guardfile
+++ Guardfile
@@ -5,5 +5,7 @@ guard :rspec do
   watch(%r{^spec/.+_spec\.rb$})
   watch(%r{^lib/(.+)\.rb$})     { |m| "spec/lib/#{m[1]}_spec.rb" }
   watch('spec/spec_helper.rb')  { "spec" }
+
+  notification :libnotify, :timeout => 5, :transient => true, :append => false, :urgency => :critical
 end
 
--- spec/lib/hero_spec.rb
+++ spec/lib/hero_spec.rb
@@ -2,9 +2,10 @@ require 'spec_helper'
 require_relative '../../lib/hero'
 
 describe Hero do
-  
+  let(:dicepool) { double("dicepool") } 
+
   describe "default attribute" do
-    let(:hero) { Hero.new } 
+    let(:hero) { Hero.new dicepool: dicepool } 
     it "has default strength equal to 3" do
       expect(hero.strength).to eq(3)
     end 
@@ -14,12 +15,32 @@ describe Hero do
   end
 
   it "can be initialized with custom strength" do
-    hero = Hero.new strength: 3
+    hero = Hero.new strength: 3, dicepool: dicepool
     expect(hero.strength).to eq(3)   
   end
 
   it "can be initialized with custom health" do
-    hero = Hero.new health: 8
+    hero = Hero.new health: 8, dicepool: dicepool
     expect(hero.health).to eq(8)   
+  end
+
+  describe "attack attack" do
+    it "succeeds" do
+      dicepool.stub(:skill_check).and_return(3)
+      hero = Hero.new dicepool: dicepool
+      monster = double("Monster", toughness: 2)
+      
+      expect(hero.attack(monster)).to be_true
+
+    end
+    it "fails" do
+      dicepool.stub(:skill_check).and_return(1)
+      hero = Hero.new dicepool: dicepool
+      monster = double("Monster", toughness: 2)
+
+      expect(hero.attack(monster)).to be_false
+
+    end
+    
   end
 end
