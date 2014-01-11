4d194f56f1c6e18a83fdfd76e682e40111b06022
Hafiz <ibnukamy@gmail.com>
2014-01-11 15:44:55 +0800

    AttackAction effect

--- spec/lib/attack_action_spec.rb
+++ spec/lib/attack_action_spec.rb
@@ -2,19 +2,48 @@ require 'spec_helper'
 require_relative '../../lib/attack_action'
 
 describe AttackAction do
-  let(:hero) { double("hero", strength: 3) } 
+  let(:hero) { double("hero", strength: 3, gain_exp: nil, gain_gold: nil, damage: nil) } 
   let(:action) { AttackAction.new hero, dicepool }
   let(:dicepool) { double("dicepool") } 
+  let(:monster) { double("monster", toughness: 2, kill: nil, damage: nil) } 
+
+  describe "effect" do
+    context "success" do
+      before :each do
+        dicepool.stub(:skill_check).and_return(true)
+      end
+      
+      it "kills monster" do
+        monster.should_receive(:kill)
+        action.activate(monster)      
+      end
+      it "rewards owner with exp" do
+        hero.should_receive(:gain_exp)    
+        action.activate(monster)      
+      end
+
+      it "rewards owner with gold" do
+        hero.should_receive(:gain_gold)
+        action.activate(monster)
+      end   
+    end
+
+    context "failure" do
+      it "damages owner" do
+        dicepool.stub(:skill_check).and_return(false)
+        hero.should_receive(:damage).with(monster.damage)
+        action.activate(monster)        
+      end
+    end    
+  end
 
   describe "activate" do
     
     it "makes strength check against target toughness" do
-      monster = double("monster", toughness: 2)
       dicepool.should_receive(:skill_check).with(hero.strength, monster.toughness)
       action.activate(monster)
     end
   end
-
 
   it "responds to activate message" do
     expect(action).to respond_to(:activate)
@@ -23,6 +52,4 @@ describe AttackAction do
   it "has an owner" do
     expect(action.owner).to eq(hero)
   end
-
-
 end
