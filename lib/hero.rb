919f9434117e0d2e7c60996f721057aa0328186f
Hafiz <ibnukamy@gmail.com>
2014-01-11 16:14:20 +0800

    Added needed attributes and methods for AttackAction to work

--- lib/hero.rb
+++ lib/hero.rb
@@ -1,13 +1,27 @@
 class Hero
-  attr_reader :strength, :health, :actions
+  attr_reader :strength, :health, :actions, :gold, :exp
 
   def initialize(attr = {})
     @strength = attr.fetch(:strength, 3)
     @health = attr.fetch(:health, 10)
     @actions = attr.fetch(:actions, {})
+    @gold = 0
+    @exp = 0
   end
 
   def activate_action(action_name, target)
     actions[action_name].activate(target)
+  end
+
+  def gain_exp(amount)
+    @exp += amount
+  end
+
+  def gain_gold(amount)
+    @gold += amount
+  end
+
+  def damage(hit)
+    @health -= hit
   end
 end
