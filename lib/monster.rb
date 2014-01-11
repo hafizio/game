f076f83962a896a7c75a394ac22034067bd2d7dc
Hafiz <ibnukamy@gmail.com>
2014-01-11 17:49:57 +0800

    Monster implementation

--- lib/monster.rb
+++ lib/monster.rb
@@ -1,10 +1,21 @@
 class Monster
-  attr_reader :toughness, :damage, :kill
+  attr_reader :toughness, :damage, :notice, :gold, :exp
 
-  def initialize
-    @toughness = 10
-    @damage = 3
+  def initialize(attr = {})
+    @toughness = attr.fetch(:toughness, 2)
+    @damage = attr.fetch(:damage, 2)
+    @notice = attr.fetch(:notice, 2)
+    @gold = attr.fetch(:gold, 10)
+    @exp = attr.fetch(:exp, 20)
+
+    @dead = false
   end
 
+  def dead?
+    @dead
+  end
 
+  def kill
+    @dead = true
+  end
 end
