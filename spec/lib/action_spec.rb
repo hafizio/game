73f41935a3cd63d91a38a7469fadfd7b4e3401c8
Hafiz <ibnukamy@gmail.com>
2014-04-25 02:00:06 +0800

    Test the behaviour of a parent class: Action

--- spec/lib/action_spec.rb
+++ spec/lib/action_spec.rb
@@ -15,4 +15,22 @@ describe Action do
   let(:monster) { double("monster", toughness: 2, kill: nil, damage: nil) } 
 
   it_behaves_like "action"
+
+  it 'requires action attributes to be implemented' do
+    expect { Action.new hero, dicepool }.to raise_exception
+  end
+
+  describe "activate" do
+    it "send success message if skill check is successful" do
+      dicepool.stub(:skill_check).and_return(true)
+      action.should_receive(:success)
+      action.activate(monster)
+    end
+
+    it "send failure message if skill check is not successful" do
+      dicepool.stub(:skill_check).and_return(false)
+      action.should_receive(:failure)
+      action.activate(monster)
+    end
+  end
 end
